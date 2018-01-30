ba6b5b2b49b9da7d191aceddf1a17343f28c8413
Scarlett Lee <scarlett8152@gmail.com>
2018-01-30 11:38:53 +0900

    added ouput "Tell His world"

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hellogit{
 	public static void main(String[] args){
 		System.out.println("Hello git");
 		System.out.println("Tell your World");
+		System.out.println("Tell Her World");
 	}
 }
 
